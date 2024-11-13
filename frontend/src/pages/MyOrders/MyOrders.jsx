965c5005282703a28526fd89678ab51250c4fd84
Subham <isubhamgeek111@gmail.com>
2024-11-13 14:05:52 +0530

    update

--- frontend/src/pages/MyOrders/MyOrders.jsx
+++ frontend/src/pages/MyOrders/MyOrders.jsx
@@ -1,52 +1,50 @@
-import React, { useContext, useEffect, useState } from 'react'
-import './MyOrders.css'
-import axios from 'axios'
+import React, { useContext, useEffect, useState } from 'react';
+import './MyOrders.css';
+import axios from 'axios';
 import { StoreContext } from '../../Context/StoreContext';
 import { assets } from '../../assets/assets';
+import { BACKEND_URL } from '../../../utils';  // Import the backend URL from utils
 
 const MyOrders = () => {
-  
-  const [data,setData] =  useState([]);
-  const {url,token,currency} = useContext(StoreContext);
+
+  const [data, setData] = useState([]);
+  const { token, currency } = useContext(StoreContext);
 
   const fetchOrders = async () => {
-    const response = await axios.post(url+"/api/order/userorders",{},{headers:{token}});
-    setData(response.data.data)
-  }
+    // Use the BACKEND_URL imported from utils.js
+    const response = await axios.post(`${BACKEND_URL}/api/order/userorders`, {}, { headers: { token } });
+    setData(response.data.data);
+  };
 
-  useEffect(()=>{
+  useEffect(() => {
     if (token) {
       fetchOrders();
     }
-  },[token])
+  }, [token]);
 
   return (
     <div className='my-orders'>
       <h2>My Orders</h2>
       <div className="container">
-        {data.map((order,index)=>{
-          return (
-            <div key={index} className='my-orders-order'>
-                <img src={assets.parcel_icon} alt="" />
-                <p>{order.items.map((item,index)=>{
-                  if (index === order.items.length-1) {
-                    return item.name+" x "+item.quantity
-                  }
-                  else{
-                    return item.name+" x "+item.quantity+", "
-                  }
-                  
-                })}</p>
-                <p>{currency}{order.amount}.00</p>
-                <p>Items: {order.items.length}</p>
-                <p><span>&#x25cf;</span> <b>{order.status}</b></p>
-                <button onClick={fetchOrders}>Track Order</button>
-            </div>
-          )
-        })}
+        {data.map((order, index) => (
+          <div key={index} className='my-orders-order'>
+            <img src={assets.parcel_icon} alt="" />
+            <p>
+              {order.items.map((item, idx) => (
+                idx === order.items.length - 1
+                  ? `${item.name} x ${item.quantity}`
+                  : `${item.name} x ${item.quantity}, `
+              ))}
+            </p>
+            <p>{currency}{order.amount}.00</p>
+            <p>Items: {order.items.length}</p>
+            <p><span>&#x25cf;</span> <b>{order.status}</b></p>
+            <button onClick={fetchOrders}>Track Order</button>
+          </div>
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default MyOrders
+export default MyOrders;
