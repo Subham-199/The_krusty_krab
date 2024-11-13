655ec17c59dc6ca19d1ac7bd0779ff0f296d651f
Subham <isubhamgeek111@gmail.com>
2024-11-13 22:18:47 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -1,35 +1,41 @@
-import express  from "express"
-import cors from 'cors'
-import { connectDB } from "./config/db.js"
-import userRouter from "./routes/userRoute.js"
-import foodRouter from "./routes/foodRoute.js"
-import 'dotenv/config'
-import cartRouter from "./routes/cartRoute.js"
-import orderRouter from "./routes/orderRoute.js"
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+import userRouter from "./routes/userRoute.js";
+import foodRouter from "./routes/foodRoute.js";
+import 'dotenv/config';
+import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRoute.js";
 
 // app config
-const app = express()
+const app = express();
 const port = process.env.PORT || 4000;
 
-
 // middlewares
-app.use(express.json())
+app.use(express.json());
 app.use(cors({
-   origin:[process.env.FRONTEND_URL,"https://thekrustykrab.vercel.app"]
-}))
+   origin: [process.env.FRONTEND_URL, "https://thekrustykrab.vercel.app"],
+   credentials: true // Allow credentials such as cookies
+}));
+
+// Handle preflight requests
+app.options("*", cors({
+   origin: [process.env.FRONTEND_URL, "https://thekrustykrab.vercel.app"],
+   credentials: true
+}));
 
 // db connection
-connectDB()
+connectDB();
 
 // api endpoints
-app.use("/api/user", userRouter)
-app.use("/api/food", foodRouter)
-app.use("/images",express.static('uploads'))
-app.use("/api/cart", cartRouter)
-app.use("/api/order",orderRouter)
+app.use("/api/user", userRouter);
+app.use("/api/food", foodRouter);
+app.use("/images", express.static('uploads'));
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
 
 app.get("/", (req, res) => {
-    res.send("API Working")
-  });
+    res.send("API Working");
+});
 
-app.listen(port, () => console.log(`Server started on http://localhost:${port}`))
+app.listen(port, () => console.log(`Server started on http://localhost:${port}`));
