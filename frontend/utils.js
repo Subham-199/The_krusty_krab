f229d3eb4c03fbf143a70def3cc61f2348cd2278
Subham <isubhamgeek111@gmail.com>
2024-11-13 14:46:14 +0530

    update

--- frontend/utils.js
+++ frontend/utils.js
@@ -1,3 +1,1 @@
-export const BACKEND_URL = import.meta.env.VITE_RENDER_URL || 'http://localhost:4000';
-export const CURRENCY = '₹';
-export const DELIVERY_CHARGE = 80;
+export const BACKEND_URL = import.meta.env.VITE_RENDER_URL;
