be3341902c79471c768c91065575ef6fc9e94336
Subham <isubhamgeek111@gmail.com>
2024-11-13 20:53:29 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,9 @@ const port = process.env.PORT || 4000;
 
 // middlewares
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+   origin:[process.env.FRONTEND_URL,"https://thekrustykrab.vercel.app"]
+}))
 
 // db connection
 connectDB()
